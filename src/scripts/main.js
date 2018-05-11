29eb3cb0064ca7acfadb5d7fa12a5093af815718
raulghm <raulghm@gmail.com>
2018-05-11 15:02:44 -0300

    feat(): agrega scroll arrow down

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,16 @@
-var slider = tns({
-  container: ".Slides",
-  mode: "gallery",
+const slider = tns({
+  container: '.Slides',
+  mode: 'gallery',
   controls: false,
   nav: false,
   items: 1,
   autoplay: true
-});
+})
+
+const arrow = document.querySelector('.Home-down')
+
+arrow.addEventListener('click', () => {
+  document.querySelector('.Home-section--next').scrollIntoView({
+    behavior: 'smooth'
+  })
+})
