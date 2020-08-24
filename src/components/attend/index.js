a4ca3dc7002ad4b4102834095c5b5b00936ab2e6
Raúl <raulghm@gmail.com>
2020-08-24 11:33:43 -0400

    fix: soluciona evento gcalendar

--- src/components/attend/index.js
+++ src/components/attend/index.js
@@ -9,7 +9,7 @@ function Attend() {
         <div className="text-sm">Agosto . 19:00 hrs</div>
       </div>
       <div className="flex flex-row ml-4">
-        <a href="https://calendar.google.com/event?action=TEMPLATE&tmeid=NGZmdHQ1OGV2Z2U4MDBuZmdkcmdzcmx0N2ggcmF1bGdobUBt&tmsrc=raulghm%40gmail.com">
+        <a target="_blank" href="https://www.google.com/calendar/render?action=TEMPLATE&text=BeerJS+Valdivia+-+Agosto&details=%C3%9Anete+desde+el+siguiente+enlace+a+BeerJS+Valdivia%3A+https%3A%2F%2Fmeet.jit.si%2Fbeerjsvaldivia%0A%0AEs+un+meetup+normal+y+abierto+v%C3%ADa+jit.si+por+lo+que+te+solicitamos+lo+siguiente%3A%0A%0A-+Mantener+tu+micr%C3%B3fono+silenciado.%0A-+Ingresar+tu+nombre+para+identificarte+%28settings%29.%0A-+Si+quieres+hablar+o+intervenir+con+alg%C3%BAn+comentario+lo+puedes+hacer+levantando+la+mano+%28icono%29.%0A-+Si+es+necesario+puedes+usar+el+chat+o+video.%0A-+Tener+tu+cerveza+heladita+a+mano+y+disfrutar+%3B%29&location=https%3A%2F%2Fmeet.jit.si%2Fbeerjsvaldivia&dates=20200827T230000Z%2F20200828T010000Z">
           <h4 className="flex items-center justify-center rounded-lg cursor-pointer bg-primary">
             <div className="p-2 text-xl title text-background">Asistiré</div>
           </h4>
