232f3d3246e67155cb386bc5af6b9e25dee4e78d
Raúl <raulghm@gmail.com>
2020-10-29 18:53:59 -0300

    fix: actualiza link meetup

--- src/components/attend/index.js
+++ src/components/attend/index.js
@@ -9,9 +9,9 @@ function Attend() {
         <div className="text-sm">Octubre . 19:00 hrs</div>
       </div>
       <div className="flex flex-row ml-4">
-        <a target="_blank" href="https://www.google.com/calendar/render?action=TEMPLATE&text=BeerJS+Valdivia&details=BeerJS+Valdivia+edici%C3%B3n+Octubre+2020&location=https%3A%2F%2Fmeet.jit.si%2Fbeerjsvaldivia&dates=20201029T220000Z%2F20201030T010000Z">
+        <a target="_blank" href="https://meet.jit.si/beerjsvaldivia">
           <h4 className="flex items-center justify-center rounded-lg cursor-pointer bg-primary">
-            <div className="p-2 text-xl title text-background">Asistiré</div>
+            <div className="p-2 text-xl title text-background">Ingresar al meetup</div>
           </h4>
         </a>
       </div>
