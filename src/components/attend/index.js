74abff36230c3a0ca07ae8d5e9687f5278764023
Raúl <raulghm@gmail.com>
2020-10-30 10:27:28 -0300

    fix: actualiza mensaje final

--- src/components/attend/index.js
+++ src/components/attend/index.js
@@ -4,16 +4,11 @@ import { Icon, Ribbon } from '../'
 function Attend() {
   return (
     <div className={styles.attend}>
-      <div>
-        <div className={styles.attendHeading}>Jueves . 29</div>
-        <div className="text-sm">Octubre . 19:00 hrs</div>
-      </div>
-      <div className="flex flex-row ml-4">
-        <a target="_blank" href="https://meet.jit.si/beerjsvaldivia">
-          <h4 className="flex items-center justify-center rounded-lg cursor-pointer bg-primary">
-            <div className="p-2 text-xl title text-background">Ingresar al meetup</div>
-          </h4>
-        </a>
+      <div className="">
+        <div className="p-2 rounded-lg">
+          <div className="text-primary">¡Gracias por participar de la edición de octubre! 🍺</div>
+          <div>Pronto subiremos las presentaciones de cada charla.</div>
+        </div>
       </div>
     </div >
   )
