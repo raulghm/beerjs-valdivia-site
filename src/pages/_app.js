bc355454be2d62e10c77cac36a985cf51c466ded
Raúl <raulghm@gmail.com>
2020-11-23 10:24:05 -0300

    fix: agrega attr lang a html

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import '../styles/base.css'
 
 // eslint-disable-next-line
 export default function App ({ Component, pageProps }) {
+  useEffect(() => {
+    document.documentElement.lang = 'es'
+  })
+
   return <Component {...pageProps} />
 }
